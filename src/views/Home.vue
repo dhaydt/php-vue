a94ceec851ab0f623d5b4309363d36948c1b6785
MH <1992dayat@gmail.com>
2020-12-17 13:19:51 +0700

    login pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 ml-auto mr-auto">
+          <div class="card my-4">
+            <div class="card-heading">
+              <h4 class="h card-title bg-info p-3 text-center">Admin Login</h4>
+            </div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <label for="user">Username</label>
+                    <input type="text" placeholder="Username" class="form-control" name="username">
+                  </div>
+                  <div class="form-group col-md-12">
+                    <label for="user">Username</label>
+                    <input type="password" placeholder="Password" class="form-control" name="password">
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <button class="btn btn-primary btn-sm float-right">Login</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
